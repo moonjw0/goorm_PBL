d00d4ee667d7f170e04fe88e19971c887c7fe0c9
moonjw0 <jjwon1230@naver.com>
2023-08-29 18:48:16 +0900

    feat: 내용 추가 및 수정 기능

--- 02 TodoApp/main.js
+++ 02 TodoApp/main.js
@@ -18,21 +18,38 @@ add_todo.addEventListener('click', () => { // 새로운 TODO 추가
     </div>
   `;
   const todo_input = new_todo.querySelector(".input") // 긱 TODO별 id 설정 - input
-  const todo_write = new_todo.querySelector(".write") // 긱 TODO별 id 설정 - write
-  const todo_delete = new_todo.querySelector(".delete") // 긱 TODO별 id 설정 - delete
-  todo_input.id = todo_list.length + 1;
-  todo_write.id = todo_list.length + 1;
-  todo_delete.id = todo_list.length + 1;
+  todo_input.id = todo_list.length; // id 0번부터 시작
 
+  todo_list.push('');
   add_loc.appendChild(new_todo);
 })
 
-add_loc.addEventListener('click', (event) => { // TODO 내용 삭제
 
+add_loc.addEventListener('click', (event) => { // TODO 내용 추가 + localStorage 저장
+  if(event.target.className === 'write'){
+    const todo_input = event.target.parentNode.previousElementSibling.querySelector(".input"); // write 버튼과 같은 영역의 input 값
+    if(todo_input){
+      const inputvalue = todo_input.value; 
+      const inputid = todo_input.id;
+
+      localStorage.setItem(inputid, inputvalue);
+      todo_list[inputid] = inputvalue; 
+    }
+  }
+})
+
+
+add_loc.addEventListener('click', (event) => { // TODO 내용 삭제 + localStorage 삭제
   if(event.target.className === 'delete'){
-    console.log('hello');
     const remove_todo = event.target.parentNode.parentNode;
-    console.log(remove_todo);
+    const todo_input = event.target.parentNode.previousElementSibling.querySelector(".input");
+    const inputid = todo_input.id;
+
     remove_todo.remove();
+    localStorage.removeItem('inputid');
+    // todo_list.splice(inputid, 1);
+    // splice 쓰려면 inputid 값 바꾸는 법 필요
+    delete todo_list[inputid];
+    console.log(todo_list);
   }
 })
