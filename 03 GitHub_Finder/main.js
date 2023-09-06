14528148b775f5f4fee64629dbeb0f29a4e796e6
moonjw0 <jjwon1230@naver.com>
2023-09-06 14:23:39 +0900

    fix: footer 고정, 사용자없음 표시 수정

--- 03 GitHub_Finder/main.js
+++ 03 GitHub_Finder/main.js
@@ -6,22 +6,28 @@ class GitHubAPI {
   async getUser(username){ // username으로 api 조회
     const response = await fetch(`${this.URL}/users/${username}`); // 사용자 정보 요청
 
-    if(!response.ok) {
-      throw new Error('Error fetching data: ${response.statusText')
+    if(response.status === 404) {
+      console.log(`Error fetching data: ${response.statusText}`);
+      return '';
     }
-    const data = await response.json(); // json형식으로 파싱
-    return data;
+    else{
+      const data = await response.json(); // json형식으로 파싱
+      return data;
+    }
   }
 
   async getRepos(username){ // username으로 api 조회
     const response = await fetch(`${this.URL}/users/${username}/repos`); // 사용자 정보 요청
 
-    if(!response.ok) {
-      throw new Error('Error fetching data: ${response.statusText')
+    if(response.status === 404) {
+      console.log(`Error fetching data: ${response.statusText}`);
+      return '';
     }
-    const repos = await response.json(); // json형식으로 파싱
-    repos.sort((a, b) => new Date(b.created_at) - new Date(a.created_at));
-    return repos;
+    else{
+      const repos = await response.json(); // json형식으로 파싱
+      repos.sort((a, b) => new Date(b.created_at) - new Date(a.created_at));
+      return repos;
+    }
   }
 }
 
@@ -35,16 +41,16 @@ function SearchProfile(event){
 
       userinfo.getUser(name)
       .then(data => {
-        document.getElementById('pic').src = data.avatar_url;
+        document.getElementById('pic').src = data.avatar_url ? data.avatar_url : './assets/user.png';
         document.getElementById('public_repos').textContent = `Public Repos: ${data.public_repos}`;
         document.getElementById('public_gists').textContent = `Public Gists: ${data.public_gists}`;
         document.getElementById('followers').textContent = `Followers: ${data.followers}`;
         document.getElementById('followings').textContent = `Followings: ${data.following}`;
 
-        document.getElementById('company').innerHTML = data.company ? `Company: <strong>${data.company}</strong>` : 'Company: <strong>No Information</strong>';
-        document.getElementById('blog').innerHTML = data.blog ? `Website/Blog: <strong>${data.blog}</strong>` : 'Website/Blog: <strong>No Information</strong>';
-        document.getElementById('location').innerHTML = data.location ? `Location: <strong>${data.location}</strong>` : 'Location: <strong>No Information</strong>';
-        document.getElementById('member_since').innerHTML = `Member Since: <strong>${data.created_at.split('T')[0]}</strong>`;
+        document.getElementById('company').innerHTML = data.company ? `Company: <strong>${data.company}</strong>` : 'Company: ' + NoInformation;
+        document.getElementById('blog').innerHTML = data.blog ? `Website/Blog: <strong>${data.blog}</strong>` : 'Website/Blog: ' + NoInformation;
+        document.getElementById('location').innerHTML = data.location ? `Location: <strong>${data.location}</strong>` : 'Location: ' + NoInformation;
+        document.getElementById('member_since').innerHTML = data.created_at ? `Member Since: <strong>${data.created_at.split('T')[0]}</strong>` : 'Member Since: ' + NoInformation;
       })
       .catch(error => {
         console.log('There was an error fetching the data: ', error);
@@ -57,15 +63,19 @@ function SearchRepos(event){
   if(event.keyCode === 13){
     const name = event.target.value.trim();
     const footer = document.getElementById('footer');
+    
     if(name) {
       const repo_container = document.getElementById('repo-container');
-      const userrepos = new GitHubAPI();
       repo_container.innerHTML = '';
-
+      
+      const userrepos = new GitHubAPI();
       userrepos.getRepos(name)
       .then(repos => {
-        if (repos.length >= 4){
+        if(repos.length !== 0){
           footer.style.position = 'sticky';
+        }
+        else{
+          footer.style.position = 'fixed';
         }
         repos.forEach(repo => {
         let repolist = document.createElement('div');
